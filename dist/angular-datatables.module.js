9b45214c47adf31baea523d872fc9328a29eeada
Louis LIN <louis.lin.87@gmail.com>
2017-02-06 22:08:11 +0100

    [WIP] Update demo dep versions

--- dist/angular-datatables.module.js
+++ dist/angular-datatables.module.js
@@ -10,18 +10,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 import { NgModule } from '@angular/core';
 import { DataTableDirective } from './angular-datatables.directive';
-var DataTablesModule = (function () {
+export var DataTablesModule = (function () {
     function DataTablesModule() {
     }
+    DataTablesModule = __decorate([
+        NgModule({
+            declarations: [DataTableDirective],
+            exports: [DataTableDirective]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], DataTablesModule);
     return DataTablesModule;
 }());
-DataTablesModule = __decorate([
-    NgModule({
-        declarations: [DataTableDirective],
-        exports: [DataTableDirective]
-    })
-], DataTablesModule);
-export { DataTablesModule };
 //# sourceMappingURL=angular-datatables.module.js.map
